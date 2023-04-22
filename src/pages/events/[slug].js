a52967fc8de736c4361c4226e717a183f25a2350
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-04-22 06:47:36 +0200

    Add Layout and clean css

--- src/pages/events/[slug].js
+++ src/pages/events/[slug].js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import Layout from "@/components/Layout";
+
 export default function EventPage() {
   return (
-    <div>
+    <Layout>
       <h1>My Event</h1>
-    </div>
+    </Layout>
   );
 }
